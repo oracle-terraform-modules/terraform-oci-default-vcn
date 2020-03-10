c2d6b0b93ef0d2c3b6cba86a7b82ee3a3f8fd34f
Marco Greiveldinger <marco@greiveldinger.de>
2020-03-10 07:19:56 +0100

    update to hcl2 syntax

--- outputs.tf
+++ outputs.tf
@@ -2,30 +2,30 @@
 
 output "vcn_id" {
   description = "ocid of created VCN. "
-  value       = "${oci_core_vcn.this.id}"
+  value       = oci_core_vcn.this.id
 }
 
 output "default_security_list_id" {
   description = "ocid of default security list. "
-  value       = "${oci_core_vcn.this.default_security_list_id}"
+  value       = oci_core_vcn.this.default_security_list_id
 }
 
 output "default_dhcp_options_id" {
   description = "ocid of default DHCP options. "
-  value       = "${oci_core_vcn.this.default_dhcp_options_id}"
+  value       = oci_core_vcn.this.default_dhcp_options_id
 }
 
 output "default_route_table_id" {
   description = "ocid of default route table. "
-  value       = "${oci_core_vcn.this.default_route_table_id}"
+  value       = oci_core_vcn.this.default_route_table_id
 }
 
 output "internet_gateway_id" {
   description = "ocid of internet gateway. "
-  value       = "${oci_core_internet_gateway.this.id}"
+  value       = oci_core_internet_gateway.this.id
 }
 
 output "subnet_ids" {
   description = "ocid of subnet ids. "
-  value       = "${oci_core_subnet.this.*.id}"
+  value       = oci_core_subnet.this.*.id
 }
